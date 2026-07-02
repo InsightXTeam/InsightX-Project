0587e8683d1eceaf24ca82f3879acf78fe92e1d7
Mohammed Mashal <adelmashal2010@gmail.com>
2026-07-02 06:36:56 +0300

    feat: add Alert entity to domain layer

---
+++ BackEnd/InsightX.Domain/Entities/Alert..cs
@@ -0,0 +1,16 @@
+﻿namespace InsightX.Domain.Entities
+{
+    public class Alert
+    {
+        public int Id { get; set; }
+        public int CompanyId { get; set; }
+        public int DepartmentId { get; set; }
+        public string KPIName { get; set; }
+        public decimal CurrentValue { get; set; }
+        public decimal Threshold { get; set; }
+        public string Message { get; set; }
+        public string Recommendation { get; set; }
+        public bool SeenByOwner { get; set; }
+        public DateTime CreatedAt { get; set; }
+    }
+}
