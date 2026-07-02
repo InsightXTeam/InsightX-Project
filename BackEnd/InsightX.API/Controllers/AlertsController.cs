a1fd70308cb85857bdec40ad4ad9c3ad7b7216cb
Mohammed Mashal <adelmashal2010@gmail.com>
2026-07-02 09:37:03 +0300

    feat : Add AlertsController

---
+++ BackEnd/InsightX.API/Controllers/AlertsController.cs
@@ -0,0 +1,36 @@
+﻿using InsightX.Application.UseCases.Alerts;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace InsightX.API.Controllers
+{
+    [ApiController]
+    [Route("api/alerts")]
+    [Authorize]
+    public class AlertsController : ControllerBase
+    {
+        private readonly GetAlertsUseCase _getAlerts;
+        private readonly MarkAlertSeenUseCase _markSeen;
+
+        public AlertsController(GetAlertsUseCase getAlerts, MarkAlertSeenUseCase markSeen)
+        {
+            _getAlerts = getAlerts;
+            _markSeen = markSeen;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAlerts([FromQuery] bool? seen)
+        {
+            var companyId = int.Parse(User.FindFirst("companyId")!.Value);
+            var alerts = await _getAlerts.ExecuteAsync(companyId, seen);
+            return Ok(alerts);
+        }
+
+        [HttpPut("{id}/seen")]
+        public async Task<IActionResult> MarkSeen(int id)
+        {
+            await _markSeen.ExecuteAsync(id);
+            return NoContent();
+        }
+    }
+}
