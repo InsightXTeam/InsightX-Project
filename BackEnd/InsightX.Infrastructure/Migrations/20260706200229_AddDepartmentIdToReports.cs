daee72f50021146fd5a30b63369e55179f2108bf
Mohammed Mashal <adelmashal2010@gmail.com>
2026-07-06 23:03:34 +0300

    Add missing DepartmentId migration for Report entity

---
+++ BackEnd/InsightX.Infrastructure/Migrations/20260706200229_AddDepartmentIdToReports.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace InsightX.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddDepartmentIdToReports : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
